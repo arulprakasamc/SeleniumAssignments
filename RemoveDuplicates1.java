5a0d6fb0148d0a34311cff99daeeda44bc4cbc0d
arulprakasamc <32581685+arulprakasamc@users.noreply.github.com>
2021-05-08 13:04:59 +0530

    Add files via upload

---
+++ RemoveDuplicates1.java
@@ -0,0 +1,43 @@
+package week3.assignments;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class RemoveDuplicates1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String input = "PayPal";
+		
+		char[] chars = input.toCharArray();
+		
+		Set<Character> charSet = new LinkedHashSet<Character>();
+		
+		Set<Character> dupCharSet = new LinkedHashSet<Character>();
+		
+		for (int i = 0; i < chars.length; i++) {
+			char str = chars[i];
+			if (str != ' ') {
+				charSet.add(new Character(chars[i]));
+
+			}
+
+
+			if (charSet.contains(chars[i])) {
+				dupCharSet.add(new Character(chars[i]));
+
+			}
+
+		}
+
+		if (dupCharSet.equals(charSet)) {
+			charSet.remove(dupCharSet);
+		}
+
+
+				System.out.println(charSet);
+			
+		}
+}
+		
+	
